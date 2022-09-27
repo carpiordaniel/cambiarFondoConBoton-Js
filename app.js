ce70c862080a31573b1df3ec6c97dcc56b89a58e
carpiordaniel <carpiordaniel@gmail.com>
2022-09-26 23:46:20 -0500

    proyecto - cambiar color de fondo con click en un botón - HTML CSS Y JS

---
+++ app.js
@@ -0,0 +1,15 @@
+boton.addEventListener('click', function(){
+    let color = aleatorio()
+    console.log(body.style.backgroundColor  =  color);
+    console.log(b.style.color  =  color);
+    document.getElementById("b").innerHTML = color;
+})
+
+function aleatorio (){
+    let n1= (Math.round(((Math.random()).toFixed(6)) * 255))
+    let n2= (Math.round(((Math.random()).toFixed(6)) * 255))
+    let n3= (Math.round(((Math.random()).toFixed(6)) * 255))
+
+    return `rgb(${n1}, ${n2}, ${n3})`;
+}
+
